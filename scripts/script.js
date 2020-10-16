b0531e584455214f70916b9b0b28d4050717abc0
Luca <lucaperullo@outlook.it>
2020-10-16 10:34:51 +0200

    up

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+/*
+    Carousel
+*/
+$("#carousel-example").on("slide.bs.carousel", function (e) {
+  /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 5;
+  var totalItems = $(".carousel-item").length;
+
+  if (idx >= totalItems - (itemsPerSlide - 1)) {
+    var it = itemsPerSlide - (totalItems - idx);
+    for (var i = 0; i < it; i++) {
+      // append slides to end
+      if (e.direction == "left") {
+        $(".carousel-item").eq(i).appendTo(".carousel-inner");
+      } else {
+        $(".carousel-item").eq(0).appendTo(".carousel-inner");
+      }
+    }
+  }
+});
+// navbar
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < $("#title-background").height()) {
+      $(".navbar").removeClass("bg-dark");
+    } else {
+      $(".navbar").addClass("bg-dark");
+    }
+  });
+});
